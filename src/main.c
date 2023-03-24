64bc391f9b477dce45a38fac1747cedea4d1658c
Theodore <theodore.magna@epitech.eu>
2023-03-24 16:17:41 +0100

    [ADD] test main

--- src/main.c
+++ src/main.c
@@ -13,13 +13,25 @@
 int main(void)
 {
     room_t *room = NULL;
+    sfRenderWindow *window = NULL;
     tile_t **tile_list = NULL;
 
     tile_list = init_tile_list(tile_list);
     ASSERT_MALLOC(tile_list, 84);
+    window = sfRenderWindow_create((sfVideoMode){1920, 1080, 32},
+    "RPG", sfClose, NULL);
+    ASSERT_MALLOC(window, 84);
+    sfRenderWindow_setFramerateLimit(window, 60);
     room = get_room(room, EMPTY_R);
     ASSERT_MALLOC(room, 84);
+    print_room(room->room);
+    while (sfRenderWindow_isOpen(window)) {
+        sfRenderWindow_clear(window, sfBlack);
+        draw_room(window, room, tile_list);
+        sfRenderWindow_display(window);
+    }
     free_room(room);
     free_tile_list(tile_list);
+    sfRenderWindow_destroy(window);
     return 0;
 }
