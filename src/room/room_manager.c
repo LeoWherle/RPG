240d0fd305be9f11c162171eaf3671b982766900
Theodore <theodore.magna@epitech.eu>
2023-03-24 13:29:22 +0100

    [FIX] coding style error

--- src/room/room_manager.c
+++ src/room/room_manager.c
@@ -36,6 +36,15 @@ void print_room(char **room)
     }
 }
 
+static tile_t *init_collision(tile_t **tile, int i)
+{
+    if (i == VOID || i == WALL || i == DOOR)
+        tile[i]->collision = true;
+    else
+        tile[i]->collision = false;
+    return (tile[i]);
+}
+
 tile_t **init_tile_list(room_t *room, tile_t **tile_list)
 {
     int i = 0;
@@ -47,15 +56,12 @@ tile_t **init_tile_list(room_t *room, tile_t **tile_list)
     while (room->room[i] != NULL) {
         tile_list[i] = malloc(sizeof(tile_t));
         ASSERT_MALLOC(tile_list[i], NULL);
-        if (i == VOID || i == WALL || i == DOOR)
-            tile_list[i]->collision = true;
-        else
-            tile_list[i]->collision = false;
         tile_list[i]->texture = sfTexture_createFromFile(tiles[i], NULL);
         ASSERT_MALLOC(tile_list[i]->texture, NULL);
         tile_list[i]->img = sfSprite_create();
         ASSERT_MALLOC(tile_list[i]->img, NULL);
         sfSprite_setTexture(tile_list[i]->img, tile_list[i]->texture, sfTrue);
+        tile_list[i] = init_collision(tile_list, i);
         i++;
     }
     tile_list[i] = NULL;
