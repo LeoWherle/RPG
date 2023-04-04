5f256b366ac132a78f19609aeec20b911d6efbc0
Nathanael-Kimbembe <nathanael.kimbembe-croissant@epitech.eu>
2023-04-04 11:40:10 +0200

    ADD: collider struct and tag enum

---
+++ include/collision.h
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2023
+** myrpg
+** File description:
+** collision
+*/
+
+#include <SFML/Graphics.h>
+#include <stdbool.h>
+#include "chained_list.h"
+
+#ifndef COLLISION_H
+    #define COLLISION_H
+
+    typedef enum tag {
+        NONE,
+        SOLID,
+        HITBOX,
+        HURTBOX,
+        DEATHBOX,
+    } tag_t;
+
+    typedef struct collider {
+        sfFloatRect *hitbox;
+        tag_t type;
+        list_t *to_collide;
+        bool activated;
+        void *owner;
+    } collider_t;
+
+    collider_t *collision_check(collider_t *to_check, tag_t tag);
+    collider_t *collider_create(sfFloatRect *hitbox, tag_t type,
+                                bool activate, void *owner);
+    void collider_destroy(collider_t *collider);
+
+#endif /*COLLISION_H*/
