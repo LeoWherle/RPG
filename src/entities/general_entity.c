6c874436a3a55e49d25882f81175c009960a080d
Nathanael-Kimbembe <nathanael.kimbembe-croissant@epitech.eu>
2023-04-07 19:00:42 +0200

    [FIX] colision detection

--- src/entities/general_entity.c
+++ src/entities/general_entity.c
@@ -15,13 +15,23 @@
 
 void check_dir(entity_t *entity)
 {
+    collider_t *colh = NULL;
+    collider_t *colv = NULL;
     collider_t *col = NULL;
 
     entity->trig.left = entity->pos.x + entity->speed_vector.x;
     entity->trig.top = entity->pos.y + entity->speed_vector.y;
     col = collision_check(entity->trigger, SOLID);
     if (col) {
-        entity->speed_vector.x = 0;
-        entity->speed_vector.y = 0;
+        entity->trig.left = entity->pos.x + entity->speed_vector.x;
+        entity->trig.top = entity->pos.y;
+        colh = collision_check(entity->trigger, SOLID);
+        if (colh)
+            entity->speed_vector.x = 0;
+        entity->trig.left = entity->pos.x;
+        entity->trig.top = entity->pos.y + entity->speed_vector.y;
+        colv = collision_check(entity->trigger, SOLID);
+        if (colv)
+            entity->speed_vector.y = 0;
     }
 }
