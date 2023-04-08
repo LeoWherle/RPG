22232b5b7399f5576aaa4cf30e2477f95720d49f
Léo Rentmeister <leo.wehrle@epitech.eu>
2023-04-08 19:11:45 +0200

    [EDIT] main to include gui

--- src/main.c
+++ src/main.c
@@ -7,34 +7,86 @@
 
 #include <SFML/System.h>
 #include <stdbool.h>
+#include <unistd.h>
 #include "item.h"
 #include "room.h"
 #include "entities.h"
 #include "errorhandling.h"
+#include "my_str.h"
+#include "gui.h"
+#include "menu_values.h"
 
-bool exit_ecs(sfEvent *event)
+bool exit_ecs(window_t *window)
 {
-    if (event->type == sfEvtClosed)
+    if (window->event->type == sfEvtClosed) {
+        sfRenderWindow_close(window->window);
         return true;
+    }
     return false;
 }
 
-int main(int ac, UNUSED char **av)
+static int detect_display(char *env[])
+{
+    int i = 0;
+
+    if (env[0] == NULL)
+        return (-1);
+    while (env[i]) {
+        if (my_env_strcmp(env[i], "DISPLAY") == 0) {
+            return (0);
+        }
+        i++;
+    }
+    write(2, "No display detected :(\n", 23);
+    return (-1);
+}
+
+gui_t *create_gui(void)
+{
+    gui_t *gui = gui_create((sfVector2f){0, -0.48}, "assets/menu/options.png");
+    ASSERT_POINTER(gui, NULL);
+    gui_t *health_background = gui_sub_create(GUIPOS_HEALTH,
+"assets/menu/health_e.png", gui);
+    sfRectangleShape_setScale(health_background->shape, (sfVector2f){0.7, 0.7});
+    ASSERT_POINTER(health_background, NULL);
+    return gui;
+}
+
+item_t *item_creator(item_t *item, window_t *window)
+{
+    gui_t *gui = create_gui();
+    item = item_create(item, gui, &gui_destroy);
+    ASSERT_POINTER(item, NULL);
+    item_set_func(item, NULL, NULL, &gui_draw);
+    item = item_create(item, create_player(window), destroy_player);
+    ASSERT_MALLOC(item, NULL);
+    item_set_func(item, player_update, player_animation, player_print);
+    gui_t *hp = gui_sub_create(GUIPOS_HEALTH, "assets/menu/health_f.png", gui);
+    entity_t *player = (entity_t *)(item->item);
+    hp->type = GUI_HEALTH_BAR;
+    hp->interactor = &player->stats;
+    ASSERT_POINTER(hp, NULL);
+    item = item_create(item, create_slime(window), destroy_enemy);
+    ASSERT_MALLOC(item, NULL);
+    item_set_func(item, enemy_update, NULL, enemy_print);
+    item = item_create(item, init_map(VILLAGE_R), free_map);
+    ASSERT_MALLOC(item, NULL);
+    item_set_func(item, NULL, NULL, draw_room);
+    return item;
+}
+
+int main(int ac, UNUSED char *av[], char *env[])
 {
     window_t *window = NULL;
     item_t *item = NULL;
 
-    if (ac != 1)
+    if (ac != 1 || detect_display(env) == -1)
         return 84;
     window = window_create((sfVideoMode){1920, 1080, 32}, 60,
             "Into the abyss", (sfFloatRect){0, 0, 1920, 1080});
-    item = item_create(item, create_player(window), destroy_player);
-    item = item_create(item, gui_create_all(window), gui_destroy_all);
+    ASSERT_MALLOC(window, 84);
+    item = item_creator(item, window);
     ASSERT_MALLOC(item, 84);
-    item_set_func(item, player_update, NULL, player_print);
-    item = item_create(item, init_map(), free_map);
-    ASSERT_MALLOC(item, 84);
-    item_set_func(item, NULL, NULL, draw_room);
     item_loop(item, window, exit_ecs);
     item_list_destroy(item);
     window_destroy(window);
