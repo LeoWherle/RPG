ade34f6a14e9708d92910312778cb01708c7f1c8
Nathanael-Kimbembe <nathanael.kimbembe-croissant@epitech.eu>
2023-04-11 14:45:23 +0200

    [ADD] doxygen and start of bow implementation

--- include/projectile.h
+++ include/projectile.h
@@ -22,12 +22,34 @@
         sfFloatRect box;
         sfVector2f pos;
         sfVector2f move_vect;
+        int power;
         void (*move)(struct projectile *, window_t *);
     } projectile_t;
 
+    /**
+     * @brief create a projectile
+     * 
+     * @param rect the rectangle of the projectile (position and size)
+     * @param move_vect the movement/speed vector of the projectile
+     * @param texture texture of the projectile
+     * @param move function pointer to move
+     * @return projectile_t*
+     */
     projectile_t *projectile_create(sfFloatRect rect, sfVector2f move_vect,
         sfTexture *texture, void (*move)(struct projectile *, window_t *));
+
+    /**
+     * @brief delete a node from the projectile list
+     * 
+     * @param node the node to be destroyed
+     */
     void projectile_delete(void *node);
+
+    /**
+     * @brief delete the projectile list
+     * 
+     * @param item the list to be destroyed
+     */
     void projectile_list_destroy(void *item);
     void projectile_move_sinus(projectile_t *proj, window_t *win);
     void projectile_move_line(projectile_t *proj, window_t *win);
---
+++ src/weapon/bow/bow_use.c
@@ -0,0 +1,7 @@
+/*
+** EPITECH PROJECT, 2023
+** myrpg
+** File description:
+** use function for bow
+*/
+
